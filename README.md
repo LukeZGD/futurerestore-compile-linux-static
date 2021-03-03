f1bebc61811e3b9f69fd498e8b5b89e0d415eee1
LukeZGD <l32luke@gmail.com>
2021-03-03 11:21:50 +0800

    libcurl now also static

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # futurerestore-compile-linux-static
 
 - Compiles a "static" build of futurerestore for Linux
-- By "static", I only mean libzip, libimobiledevice libs (libplist, libusbmuxd, libimobiledevice), and tihmstar libs (libgeneral, libfragmentzip, img4tool) needed by futurerestore
+- By "static", I mean libcurl, libzip, libimobiledevice libs (libplist, libusbmuxd, libimobiledevice), and tihmstar libs (libgeneral, libfragmentzip, img4tool) needed by futurerestore
 - Got a lot of the stuff needed from opa334's Windows compile script, thanks to opa334 for the script
 - Script is tested on Xubuntu 20.04.2
--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ cd futurerestore_compile
 
 sudo add-apt-repository universe
 sudo apt update
-sudo apt install -y pkg-config libtool automake g++ python-dev libzip-dev libcurl4-openssl-dev cmake libssl-dev libusb-1.0-0-dev libreadline-dev libbz2-dev libpng-dev git autopoint aria2
+sudo apt install -y pkg-config libtool automake g++ python-dev cmake libssl-dev libusb-1.0-0-dev libreadline-dev libbz2-dev libpng-dev git autopoint aria2
 
 git clone https://github.com/lzfse/lzfse
 git clone https://github.com/libimobiledevice/libplist
@@ -19,8 +19,9 @@ git clone https://github.com/libimobiledevice/libirecovery
 git clone https://github.com/tihmstar/libgeneral
 git clone https://github.com/tihmstar/libfragmentzip
 git clone https://github.com/tihmstar/img4tool
-git clone https://github.com/madler/zlib
 git clone --recursive https://github.com/marijuanARM/futurerestore
+git clone https://github.com/madler/zlib
+git clone https://github.com/curl/curl
 aria2c https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz
 aria2c https://tukaani.org/xz/xz-5.2.4.tar.gz
 aria2c https://libzip.org/download/libzip-1.5.1.tar.gz
@@ -29,60 +30,70 @@ aria2c https://libzip.org/download/libzip-1.5.1.tar.gz
 sed -i'' 's|#   include CUSTOM_LOGGING|//#   include CUSTOM_LOGGING|' libgeneral/include/libgeneral/macros.h
 
 cd lzfse
-sudo make install LDFLAGS="$BEGIN_LDFLAGS"
+make LDFLAGS="$BEGIN_LDFLAGS"
+sudo make install
 cd ..
 
 cd libplist
 ./autogen.sh $STATIC_FLAG --without-cython
-sudo make install LDFLAGS="$BEGIN_LDFLAGS"
+make LDFLAGS="$BEGIN_LDFLAGS"
+sudo make install
 cd ..
 
 cd libusbmuxd
 ./autogen.sh $STATIC_FLAG
+make LDFLAGS="$BEGIN_LDFLAGS"
 sudo make install LDFLAGS="$BEGIN_LDFLAGS"
 cd ..
 
 cd libimobiledevice
 ./autogen.sh $STATIC_FLAG --without-cython
-sudo make install LDFLAGS="$BEGIN_LDFLAGS"
+make LDFLAGS="$BEGIN_LDFLAGS"
+sudo make install
 cd ..
 
 cd libirecovery
 ./autogen.sh $STATIC_FLAG
-sudo make install LDFLAGS="$BEGIN_LDFLAGS"
+make LDFLAGS="$BEGIN_LDFLAGS"
+sudo make install
 cd ..
 
 cd libgeneral
 ./autogen.sh $STATIC_FLAG
-sudo make install LDFLAGS="$BEGIN_LDFLAGS"
+make LDFLAGS="$BEGIN_LDFLAGS"
+sudo make install
 cd ..
 
 cd libfragmentzip
 ./autogen.sh $STATIC_FLAG
-sudo make install LDFLAGS="$BEGIN_LDFLAGS"
+make LDFLAGS="$BEGIN_LDFLAGS"
+sudo make install
 cd ..
 
 cd img4tool
 ./autogen.sh $STATIC_FLAG
-sudo make install LDFLAGS="$BEGIN_LDFLAGS"
+make LDFLAGS="$BEGIN_LDFLAGS"
+sudo make install
 cd ..
 
 tar -zxvf bzip2-1.0.8.tar.gz
 cd bzip2-1.0.8
-make
-sudo make install LDFLAGS="$BEGIN_LDFLAGS"
+make LDFLAGS="$BEGIN_LDFLAGS"
+sudo make install
 cd ..
 
 cd zlib
 ./configure --static
-sudo make install LDFLAGS="$BEGIN_LDFLAGS"
+make LDFLAGS="$BEGIN_LDFLAGS"
+sudo make install
 cd ..
 
 tar -zxvf xz-5.2.4.tar.gz
 cd xz-5.2.4
 ./autogen.sh
 ./configure $STATIC_FLAG
-sudo make install LDFLAGS="$BEGIN_LDFLAGS"
+make LDFLAGS="$BEGIN_LDFLAGS"
+sudo make install
 cd ..
 
 tar -zxvf libzip-1.5.1.tar.gz
@@ -90,12 +101,22 @@ cd libzip-1.5.1
 mkdir build
 cd build
 cmake .. -DBUILD_SHARED_LIBS=OFF
-make
+make LDFLAGS="$BEGIN_LDFLAGS"
 sudo make install
 cd ../..
 
+cd curl
+autoreconf -fi
+./configure $STATIC_FLAG
+make CFLAGS="-DCURL_STATICLIB" LDFLAGS="$BEGIN_LDFLAGS"
+sudo make install
+sudo ln -sf /usr/local/lib/libcurl.a /usr/lib/x86_64-linux-gnu
+sudo ln -sf /usr/local/lib/libcurl.la /usr/lib/x86_64-linux-gnu
+sudo ln -sf /usr/local/lib/pkgconfig/libcurl.pc /usr/lib/x86_64-linux-gnu/pkgconfig
+cd ..
+
 cd futurerestore
 ./autogen.sh
-make LDFLAGS="$BEGIN_LDFLAGS" libgeneral_LIBS="-llzma -lbz2"
+make CFLAGS="-DCURL_STATICLIB" LDFLAGS="$BEGIN_LDFLAGS" libgeneral_LIBS="-llzma -lbz2"
 sudo make install
 cd ..
